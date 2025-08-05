2674d59c22a6ecc507aad0d7ffc6d17e4f1cf294
Samm <samm8gg@gmail.com>
2025-08-06 01:41:40 +0300

    Narrow Down gaps and Add Waterline Stations
    # Conflicts:
    #	data/stations_by_op.py

--- data/stations_by_op.py
+++ data/stations_by_op.py
@@ -1,23 +1,5 @@
-
-
-connect_s = ["Stepford Central", "Morganstown", "Stepford Victoria", "Stepford Airport Central","Leighton West", "Llyn-by-the-sea","Leighton City", "Leighton Stepford Road", "Leighton Buzzard", "Leighton West", "Leighton City", "Leighton Stepford Road","St helens bridge","Port Benton","Beechley","Airport Terminal 2","Whitefield","Willowfield","Edgemead","Benton","Rayleigh Bay"]
-
-
-
-
-airlink_s =  ["Stepford Central","Stepford Airport Central","Airport Terminal 2","Airport Terminal 3","Leighton Stepford Road"]
-
-
-
-waterline_s = ["Newry","Newry Harbour","Greenslade","Connolly","Benton","Morganstown","Airport Terminal 2"]
-
-
-
-
-metro_s = ["Stepford Central", "Barton","Willowfield","St Helens Bridge","Beechley","Stepford Victoria","Berrily","Morganstown","Port Benton","Stepford United Football Club"]
-
-
-
-
-express_s = ["Stepford Centeral","Benton","Llyn-by-the-sea","Leighton City","Newry","Newry Harbour","Rayleigh Bay"]
-
+Connect_S = ["Stepford Central", "Morganstown", "Stepford Victoria", "Stepford Airport Central","Leighton West", "Llyn-by-the-sea","Leighton City", "Leighton Stepford Road", "Leighton Buzzard", "Leighton West", "Leighton City", "Leighton Stepford Road","St helens bridge","Port Benton","Beechley","Airport Terminal 2","Whitefield","Willowfield","Edgemead","Benton","Rayleigh Bay"]
+AirLink_S =  ["Stepford Central","Stepford Airport Central","Airport Terminal 2","Airport Terminal 3","Leighton Stepford Road"]
+Waterline_S = ["Newry Harbour","Newry","Eden Quay","Faraday Road","West Benton","Benton","Port Benton","Morganstown Docks","Morganstown","Whitney Green","Greenslade","Cambridge St. Parkway","Ashlan Park","Connolly","Airport West","Terminal 2","James Street","Farleigh","Rosedala Village","Esterfield"]
+Metro_S = ["Stepford Central", "Barton","Willowfield","St Helens Bridge","Beechley","Stepford Victoria","Berrily","Morganstown","Port Benton","Stepford United Football Club"]
+Express_S = ["Stepford Centeral","Benton","Llyn-by-the-sea","Leighton City","Newry","Newry Harbour","Rayleigh Bay"]
